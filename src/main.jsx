254508c6524977bf26d188f525484934db9cf2aa
saravana716 <vsaravana716@gmail.com>
2026-04-12 14:01:49 +0530

    ui updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { WishlistProvider } from './context/WishlistContext.jsx'
